19529938b0199565e4f3257a8f20124520bb6922
Janos Perge <janos.perge@gmail.com>
2025-01-21 12:16:06 +0100

    Add primevue, primevue themes, modify default route to CarServiceList.vue

---
+++ resources/js/Pages/CarServiceList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>Car service list</div>
+    <DataTable :value="products" tableStyle="min-width: 50rem">
+    <Column field="code" header="Code"></Column>
+    <Column field="name" header="Name"></Column>
+    <Column field="category" header="Category"></Column>
+    <Column field="quantity" header="Quantity"></Column>
+</DataTable>
+</template>
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';             // optional
+import { onMounted, ref } from 'vue';
+
+const initialProducts = [{
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},{
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+}]
+
+const products = ref()
+
+onMounted(()=>{
+    products.value = initialProducts;
+})
+
+</script>
+<style lang="scss"></style>
