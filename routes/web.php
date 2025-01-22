af9f2d62a844a4888ba7a0b2381b61b1025dae6c
Janos Perge <janos.perge@gmail.com>
2025-01-22 09:36:06 +0100

    remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\CarController;
@@ -11,20 +9,8 @@ Route::get('/', function () {
     return Inertia::render('CarServiceList');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('/api/clients', [ClientController::class, 'getClients']);
 
 Route::get('/api/filterClients', [ClientController::class, 'filterClients']);
 
 Route::get('/api/carsByClient', [CarController::class, 'getCarsByClientId']);
-
-require __DIR__ . '/auth.php';
